ee28826f9a088e61a9c11264f0125ef4b23e9b88
unknown <hexcordez@gmail.com>
2019-04-12 10:09:01 +0800

    Location layout

--- resources/views/location.blade.php
+++ resources/views/location.blade.php
@@ -3,8 +3,75 @@
 
   <div class="mx-auto col-md-10 offset-md-1 pt-4" style="width:100%">
       <h3 class="bg-primary text-white p-2 mt-4">
-    City Hall Vicinity Map
+    Location and Topology
       </h3>
-    <img src="{{URL::asset('image/cityhall.png')}}" style="width:80%;" class="mx-auto d-block">
+      <div class="row">
+          <div class="media mt-2 p-3">
+
+                <img src="{{URL::asset('image/old_spc.jpg')}}" alt="SPC" height="150px" class="align-self-start mr-2">
+                <div class="media-body">
+                  <h4 class="mt-0 font-weight-bold" style="color:3490DC;">Geographical Location</h4>
+                    <p>
+                      San Pablo City is located at the southern tip of the Province of  Laguna in the island of Luzon at geographic coordinates, 140 4’ north latitude and 1210 19’ east longtitude.  It is bounded by 6 municipalities and 1 city  namely Calauan, Laguna in the northwest;  Nagcarlan, Laguna in the northeast; Alaminos, Laguna in the west;  Rizal, Laguna in the east; Lipa City, Batangas in the southeastern tip;  and the municipalities of Tiaong and Dolores, Quezon  in the south.  All adjoining municipalities and city have entry points making San Pablo accessible from virtually all directions.
+                    </p>
+
+                    <p>
+                      Kilometerage distance from the National Capital Region is about 82 kilometers and located southwesterly of Manila.  Approximate distance with neighboring municipalities is shown in the following table 1
+                    </p>
+
+
+                </div>
+              </li>
+              </div>
+              <div class="container">
+                <h4 class="mt-0 font-weight-bold text-center pt-3" style="color:3490DC;">Approximate Distance of Neighboring Municipalities</h4>
+                <table class="table table-bordered">
+                  <thead>
+                    <tr>
+                      <th>Name of Municipality</th>
+                      <th>Distance</th>
+
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Calauan, Laguna</td>
+                      <td>9.5</td>
+
+                    </tr>
+                    <tr>
+                      <td>Alaminos, Laguna</td>
+                      <td>10.00</td>
+
+                    </tr>
+                    <tr>
+                      <td>Nagcarlan, Laguna</td>
+                      <td>14.00</td>
+                    </tr>
+                    <tr>
+                      <td>Rizal, Laguna</td>
+                      <td>14.00</td>
+                    </tr>
+                    <tr>
+                      <td>Dolores, Quezon</td>
+                      <td>14.00</td>
+                    </tr>
+                    <tr>
+                      <td>Tiaong, Quezon</td>
+                      <td>14.00</td>
+                    </tr>
+                    <tr>
+                      <td>Lipa City, Batangas</td>
+                      <td>36</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+
+            </ul>
+
+
+
+    </div>
   </div>
 <iframe src="https://www.google.com/maps/embed?pb=!1m19!1m8!1m3!1d7740.128825323812!2d121.32083646232464!3d14.073373671893519!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m5!1s0x33bd5ccf79d1fc51%3A0x3b7a30db01a7f29d!2sSan+Pablo+City+Hall!3m2!1d14.074488299999999!2d121.32454279999999!4m0!5e0!3m2!1sen!2sph!4v1553066791791" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
