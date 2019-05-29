1eb5dd3f1d5216649a803456a96bda8a2f917578
RG.xxvi-xxxi <rdimasapit.bos@gmail.com>
2019-05-29 09:16:30 +0800

    Match codes from server

--- app/Http/Controllers/GetDeptList.php
+++ app/Http/Controllers/GetDeptList.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class GetDeptList extends Controller
 {
   function getList() {
     $ls = DB::select("CALL get_depts_ls()");
-    return ["brgys" => $ls];
+    return ["depts" => $ls];
   }
 }
