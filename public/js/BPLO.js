b6c9123468df5ef88a329cabe0bfd2affc2920a3
hijax231 <jandeanpasajol231@gmail.com>
2019-08-08 14:51:16 +0800

    api

--- public/js/BPLO.js
+++ public/js/BPLO.js
@@ -1,25 +1,29 @@
+
+
   $(document).ready(function(){
 console.log('success');
     $('#addnewbuss').submit(function(e){
       e.preventDefault();
+    
+    
 
       console.log($(this).serializeArray());
       $.ajax({
-        url : 'http://192.168.100.207:8080/api/api/',
+        url : 'http://192.168.100.207:8080/api/spc_api/',
         type : 'POST',
         crossDomain: true,
         dataType: 'json',
         data : {
-          name1 : 'tes123123ting',
-          name2 : 'as1231232d'
+          'test[name1]' : '122dasds1',
+          'test[name2]' : '22asdas21'
         },
     
         success : function(res){
-          console.log(res);
-        },
-        error : function(xhr){
-          console.log(xhr.responseText);
+          res = res[0];
+          console.log(res[37].name1);
+    
         }
+       
       });
 
 
