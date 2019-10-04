7009f33c6e10ac62b71b95761366dcea35f0655a
hexccc <hexcordez@gmail.com>
2019-10-04 11:48:14 +0800

    admin up

--- resources/views/layouts/adminlogin.blade.php
+++ resources/views/layouts/adminlogin.blade.php
@@ -23,7 +23,7 @@
     <script src="//cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
     <link rel="stylesheet" href="/css/app.css">
     <link rel="stylesheet" href="/css/login.css">
-
+    <script src="/js/testing.js"></script>
 
 
 
