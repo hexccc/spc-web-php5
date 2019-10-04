d1144db465cf13b78a3082a4a9bef98d510bc276
mretcoy08 <tolentino.mcoy@gmail.com>
2019-10-04 11:57:26 +0800

    admin nav set up

---
+++ app/Http/Controllers/EventsandAnnouncementsController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EventsandAnnouncementsController extends Controller
+{
+    public function addEvents(Request $)
+}
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -23,20 +23,23 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
 
-
-
-
+ 
     {{-- <script src="/js/multistep.js"></script>
     <link rel="stylesheet" href="/css/multistep.css"> --}}
 
     <script src="/js/app.js"></script>
     <script src="/js/form.js"></script>
-    <script src="/js/BPLO.js"></script>
+    <script src="/js/admin.js"></script>
     <script src="/js/testing.js"></script>
     <script src="/js/form_renewal.js"></script>
     <script src="/js/ammendments.js"></script>
     <script src="/js/inputmask.js"></script>
     <script src="/js/jquery.bootstrap-growl.js"></script>
+    <script src="/js/BPLO.js"></script>
+    <script src="/js/jquerydatatables.js"></script>
+    <script src="/js/datatables.js"></script>
+    <script src="/js/jquery.bootstrap-growl.js"></script>
+
 
     
   </head>
