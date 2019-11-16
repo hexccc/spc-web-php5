f4b077432358233090524a4ec63290d337892590
hexccc <hexcordez@gmail.com>
2019-11-16 09:52:04 +0800

    login page redesign

--- resources/views/admin-login.blade.php
+++ resources/views/admin-login.blade.php
@@ -18,24 +18,36 @@
 
 {!! csrf_field() !!}
   <form id="signinTest" class="email-login">
- 
-    <div class="u-form-group">
-      <input type="text" placeholder="Username" name="user[username]" id="username" required autofocus/>
+    
+    <div style="height:30px;" class="form-group d-flex justify-content-center">
+      <input class="form-control my-2" type="text" placeholder="Username" name="user[username]" id="username" required autofocus/>
     </div>
-    <div class="u-form-group">
-      <input type="password" placeholder="Password" name="user[password]" id="password" required/>
+    <div style="height:30px;" class="form-group d-flex justify-content-center"">
+      <input  class="form-control my-4" type="password" placeholder="Password" name="user[password]" id="password" required/>
     </div>
-    <div class="u-form-group">
-      <button type="submit" id="signin">Log in</button>
+    <div  class="form-group d-flex justify-content-center">
+      <button class="btn btn-info btn-block btn-lg my-5" type="submit" id="signin">Log in</button>
     </form>
     </div>
-    <div class="u-form-group">
+    <div class="form-group">
       <!-- <a href="#" class="forgot-password">Forgot password?</a> -->
     </div>
- 
-</div>
-</div>
 
+</div>
+</div>
+<script type="text/javascript">
+  $(document).ready(function() {
+    $('.login-box',).css('border-radius', '25px');
+    $('.login-box',).css('width', '500px');
+    $('.login-box',).css('height', '380px');
+    $('.btn-block',).css('width', '80%');
+    $('.form-control',).css('width', '80%');
+    $('.form-control',).css('height', '150%');
+    $('.form-control',).css('border-radius', '25px');
+    $('.form-control',).css('border-width', '2px');
+    $('.btn',).css('border-radius', '25px');
+  });
+</script>
 
 
 
